7e93d1dd065843da94cf0a7229b89fef63ebec70
jafnafathima2004 <jafnafathima01@gmail.com>
2025-12-16 11:46:00 +0530

    fontstyle

--- lib/core/constant/app_colors.dart
+++ lib/core/constant/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
    static const pink = Color(0xffE43D5E);
    static const white = Color(0xffFFFFFF);
    static const lighteGrey = Color(0xffDADBE3);
+   static const black=Color(0xff1C1B1B);
 }
