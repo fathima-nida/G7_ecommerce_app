160d5f01618cc7eda6cbd881cc4ddeff0433abeb
unknown <faiha7038@gmail.com>
2025-12-16 11:55:12 +0530

    faiha

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:g7_comerce_app/features/delivery/screens/delivery_dashboard.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,8 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-     
-      home:DeliveryDashboard(),
+      home:DeliveryDashboard  (),
     );
   }
 }
