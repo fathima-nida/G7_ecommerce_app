060cf36934ed71b444350aa654597bbec6716171
unknown <fthmnida551@gmail.com>
2025-12-22 11:49:32 +0530

    mjuuh

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', home: BottomNavigationWidget());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo', home: BottomNavigationWidget());
   }
 }
