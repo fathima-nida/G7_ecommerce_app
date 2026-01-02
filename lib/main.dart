067df57b2e23d1c23033c499038b735c58448bfe
unknown <faiha7038@gmail.com>
2026-01-02 12:41:00 +0530

    faihaaa

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home:CustomerListPage(),
+      home:LoginScreen(),
     );
   }
 }
