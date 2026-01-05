b6beb7d3180822a1793427d82475bb3636f121d8
unknown <fthmnida551@gmail.com>
2026-01-05 19:18:53 +0530

    hlooooi

---
+++ lib/features/auth/domain/repositories/login_repository.dart
@@ -0,0 +1,7 @@
+import 'package:g7_comerce_app/core/commom/generic_types.dart';
+import 'package:g7_comerce_app/features/auth/domain/model/login_request_model.dart';
+import 'package:g7_comerce_app/features/auth/domain/model/login_response.dart';
+
+abstract class LoginRepository {
+  FutureEither<LoginResponseModel> LoginAuth(LoginRequestModel reqModel);
+}
