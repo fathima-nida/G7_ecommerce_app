2f39bdbd86c9b2a1df9524d1527c365eff57bba0
unknown <faiha7038@gmail.com>
2026-01-05 15:16:51 +0530

    faii

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,9 @@ import 'package:flutter/services.dart';
 import 'package:g7_comerce_app/core/utils/bottom_navigation.dart';
 import 'package:g7_comerce_app/features/auth/screens/loginscreen.dart';
 import 'package:g7_comerce_app/features/auth/screens/splashscreen.dart';
+import 'package:g7_comerce_app/features/screens/customer_choose.dart';
 import 'package:g7_comerce_app/features/screens/favourite.dart';
 import 'package:g7_comerce_app/features/screens/product%20view.dart';
-
-
 import 'package:g7_comerce_app/features/auth/screens/splashscreen.dart';
 import 'package:g7_comerce_app/features/dashboard/screens/customer_dashboard.dart';
 import 'package:g7_comerce_app/features/home/screens/searchscreen.dart';
