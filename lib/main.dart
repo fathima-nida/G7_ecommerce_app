c187a7025badb69077cd7b005af09a415e1aa2fb
unknown <faiha7038@gmail.com>
2026-01-05 12:31:30 +0530

    faiiiiiiiii

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:g7_comerce_app/core/utils/bottom_navigation.dart';
 import 'package:g7_comerce_app/features/auth/screens/loginscreen.dart';
-import 'package:g7_comerce_app/features/screens/customer_choose.dart';
-import 'package:g7_comerce_app/features/screens/favouriealert.dart';
+import 'package:g7_comerce_app/features/auth/screens/splashscreen.dart';
 import 'package:g7_comerce_app/features/screens/favourite.dart';
+import 'package:g7_comerce_app/features/screens/product%20view.dart';
+import 'package:g7_comerce_app/features/search/screens/searchscreen.dart';
+
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +25,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home:LoginScreen(),
+      home:ProductViewPage(),
+      
     );
      
   }
