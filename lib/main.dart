0afa6284a74312361974a9a51cc6a5e657481984
unknown <fthmnida551@gmail.com>
2026-01-30 14:59:00 +0530

    adding git new

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,6 @@ class MyApp extends StatelessWidget {
       BlocProvider<LoginBloc>(create: (_)=>di.getit.get<LoginBloc>()),
       BlocProvider<CstmrDashboardBloc>(create: (_)=>di.getit.get<CstmrDashboardBloc>()),
       BlocProvider(create: (_) => FavouriteBloc(FavouriteRepositoryImpl())),
-
       BlocProvider<ProductDetailsBloc>(create: (_) => di.getit.get<ProductDetailsBloc>()),
       BlocProvider<CartBloc>(create: (_)=>di.getit.get<CartBloc>()),
       BlocProvider<BannerBloc>(create: (_)=>di.getit.get<BannerBloc>()),
